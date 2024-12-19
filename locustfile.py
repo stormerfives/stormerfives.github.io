04e99eb72666f3fe124cb960a22556cdb7c555fd
Alex Ginn <88403803+stormerfives@users.noreply.github.com>
2024-12-19 12:34:03 -0500

    Setting up locustfile

--- locustfile.py
+++ locustfile.py
@@ -1,9 +1,11 @@
 from locust import HttpUser, task, between
 
 class QuickstartUser(HttpUser):
-    wait_time = between(1, 5)
-
-    @task
-    def hello_world(self):
-        self.client.get("/hello")
-        self.client.get("/world")
+    # Wait between 7 and 15 seconds per request per user
+    wait_time = between(7, 15)
+        
+    @task(1)
+    def load_test(self):
+        # Request / on your Host
+        # self.client.get("/")
+        self.client.get("/stormerfives.github.io")
